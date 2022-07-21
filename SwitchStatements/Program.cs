473ff733b5a1cc97d75074acd81ff53e01e9f319
TylerN93 <tylernewton73@gmail.com>
2022-07-21 15:37:47 -0500

    initial commit

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject");
+            
+            var response = Console.ReadLine();
+
+            switch (response)
+            {
+                case "Math":
+                    Console.WriteLine("Math is hard");
+                    break;
+                case "English":
+                    Console.WriteLine("English is cool");
+                    break ;
+                case "History":
+                    Console.WriteLine("History is a great subject");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is fun!");
+                    break;
+                case "P.E.":
+                    Console.WriteLine("That is my favorite subject!");
+                    break;
+                default:
+                    Console.WriteLine("I didnt have that class");
+                    break;
+
+
+            }
+        }
+    }
+}
